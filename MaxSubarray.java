be60fcef807e2850eea2d49640d1215113d17b73
Ayusha Burman <88042465+Ayusha-Burman@users.noreply.github.com>
2023-08-08 23:39:22 +0530

    add: Brute Force

---
+++ MaxSubarray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class MaxSubarray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter "+n+" items:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Printing subarrays:");
+        int[] subarray=new int[n];
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<n;i++)
+        { 
+            int k=0;
+            for(int j=i;j<n;j++)
+            {
+                currSum=0;
+              subarray[k]=arr[j];
+              for(int l=0;l<=k;l++)
+              {
+                currSum=currSum+subarray[l];
+              }
+              k++;
+              if(maxSum<currSum)
+              {
+                maxSum=currSum;
+              }
+
+            }
+        }
+        System.out.println("Maximum sum = "+maxSum);
+    }
+    
+}
